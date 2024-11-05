0d2f2c14ac3c7b4e463867525b53e0f417a6e4e2
Abhay Singh <abhay97ps@gmail.com>
2024-11-05 14:34:47 -0500

    build: qualtric chat interface support

--- bcfg_chat_api/urls.py
+++ bcfg_chat_api/urls.py
@@ -20,5 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('chat.urls')),
-    path('', include('chat.urls')),
 ]
--- chat/models.py
+++ chat/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class User(models.Model):
-    bcfg_id = models.BigIntegerField(unique=True)
+    bcfg_id = models.CharField(max_length=100, unique=True)
     name = models.CharField(max_length=100)
 
     def __str__(self):
